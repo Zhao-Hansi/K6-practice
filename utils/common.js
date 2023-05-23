d2b2cae69e8853bc12cb1866702316cd447be43a
zexin.zhao <zexin.zhao@thoughtworks.com>
2023-05-23 18:30:51 +0800

    compare arrow function and traditional function

--- utils/common.js
+++ utils/common.js
@@ -8,3 +8,12 @@ const randomName = (length) => {
     return result;
 }
 
+function random(length) {
+    let result = "";
+    let characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+
+    for (let i = 0; i < length; i++){
+        result = characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return result;
+}
