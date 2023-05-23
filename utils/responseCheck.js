80020803cdca96feb3039822471d5c1693c8490a
zexin.zhao <zexin.zhao@thoughtworks.com>
2023-05-23 10:38:29 +0800

    add advance sample and simplify scenario sample

---
+++ utils/responseCheck.js
@@ -0,0 +1,25 @@
+import { check } from "k6";
+
+export const checking = {
+    requestSucceedCheck: (response, message) => {
+        const handler = {};
+        const msg = `${message}, checking status code is 200`;
+        handler[msg] = (r) => r.status === 200;
+
+        check(response, handler);
+    },
+
+    loginFailedCheck: (response, message) => {
+        const handler = {};
+        const msg = `${message}, checking status code is 401`;
+        handler[msg] = (r) => r.status === 401;
+
+        check(response, handler);
+    },
+
+    loginFailedWithInternetCheck: (response, message) => {
+        const handler = {};
+        const msg = `${message}, checking status code is 502`;
+        handler[msg] = (r) => r.status === 502;
+    }
+};
