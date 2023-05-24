20bc814a6103216841e7f6d78cf6803d56802f8b
zexin.zhao <zexin.zhao@thoughtworks.com>
2023-05-24 12:57:34 +0800

    add dashboard to project and initial dashboard config

--- src/sample-script-test.ts
+++ src/sample-script-test.ts
@@ -1,5 +1,6 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
+import {metrics} from "../config/MetricsConfig";
 
 export const options = {
     scenarios: {
@@ -20,7 +21,7 @@ export const options = {
             preAllocatedVUs: 10, // the size of the VU (i.e. worker) pool for this scenario
             tags: { test_type: 'api' }, // different extra metric tags for this scenario
             env: { MY_CROC_ID: '1' }, // and we can specify extra environment variables as well!
-            exec: 'apitest', // this scenario is executing different code than the one above!
+            exec: 'webtest', // this scenario is executing different code than the one above!
         },
         my_api_test_2: {
             executor: 'ramping-arrival-rate',
@@ -40,23 +41,28 @@ export const options = {
         },
     },
     discardResponseBodies: true,
-    thresholds: {
-        // we can set different thresholds for the different scenarios because
-        // of the extra metric tags we set!
-        'http_req_duration{test_type:api}': ['p(95)<250', 'p(99)<350'],
-        'http_req_duration{test_type:website}': ['p(99)<500'],
-        // we can reference the scenario names as well
-        'http_req_duration{scenario:my_api_test_2}': ['p(99)<300'],
-    },
+    // thresholds: {
+    //     // we can set different thresholds for the different scenarios because
+    //     // of the extra metric tags we set!
+    //     'http_req_duration{test_type:api}': ['p(95)<250', 'p(99)<350'],
+    //     'http_req_duration{test_type:website}': ['p(99)<500'],
+    //     // we can reference the scenario names as well
+    //     'http_req_duration{scenario:my_api_test_2}': ['p(99)<300'],
+    // },
 };
 
 export function webtest() {
-    http.get('https://test.k6.io/contacts.php');
+    let res = http.get('https://test.k6.io/contacts.php', { tags: { endpoint: "/v1/users" } });
+    metrics.newsAPI.total_request.add(1);
+    metrics.newsAPI.requestDuration.add(res.timings.duration);
+    if (res.status !== 200){
+        metrics.newsAPI.checkFailureRate.add(1)
+    }
     sleep(Math.random() * 2);
 }
 
 export function apitest() {
-    http.get(`https://test-api.k6.io/public/crocodiles/${__ENV.MY_CROC_ID}/`);
+    http.get(`https://test-api.k6.io/public/crocodiles/${__ENV.MY_CROC_ID}/`, { tags: { endpoint: "/v2/users" } });
     // no need for sleep() here, the iteration pacing will be controlled by the
     // arrival-rate executors above!
 }
